ddca86af640c153b814ad22a721b7b3f86a305bc
mitj500 <115896953+mitj500@users.noreply.github.com>
2024-01-18 23:46:39 +0530

    Delete sum.java

--- sum.java
+++
@@ -1,9 +0,0 @@
-class sum{
-     public static void main (String Args[])
-     {
-        int a=2;
-        int b=3;
-        int res= a+b;
-        System.out.println("sum of the number is:"+res);
-     }
-}
